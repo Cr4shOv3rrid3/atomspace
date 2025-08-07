9b3b4edbdc9c7765b74f1d3f87aa4a6a92fe9748
CrashOverride <Cr4shOverride@gmx.de>
2025-08-07 20:09:56 +0200

    Update SubFrame2Frame.h
    adding the line!

--- opencog/atomspace/quantumspace/SubFrame2Frame.h
+++ opencog/atomspace/quantumspace/SubFrame2Frame.h
@@ -1,5 +1,5 @@
 //building the necessary still missing frame structure drawbridge principle thing. based on the shimLink from atom_types folder
-//contained script file.
+//contained script file (line 1083).
 //when this is done it enables using the Atomspace Frame and from there all relevant Atomic written files as a connection
 //linking too. this then no longer needs to writes additional files inside this quantumspace folder.
 //wanna limit things into the essential necessary. like those 5 lines. that now are able to act as a handshake into the future!
