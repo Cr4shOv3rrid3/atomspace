76992c1613a93a24b152ccbd3854d2578fb37fda
CrashOverride <Cr4shOverride@gmx.de>
2025-08-07 19:34:42 +0200

    Create subFrame2Frame.h
    "That's one small step for man, one giant leap for mankind." (Original Quote from Neil Armstrong  on the Apollo 11 Mission)

---
+++ opencog/atomspace/quantumspace/subFrame2Frame.h
@@ -0,0 +1,5 @@
+//building the necessary still missing frame structure drawbridge principle thing. based on the shimLink from atom_types folder
+//contained script file.
+//when this is done it enables using the Atomspace Frame and from there all relevant Atomic written files as a connection
+//linking too. this then no longer needs to writes additional files inside this quantumspace folder.
+//wanna limit things into the essential necessary. like those 5 lines. that now are able to act as a handshake into the future!
