a558260cd3dd7dd531a52e0f3cbf6d28de16d78e
CrashOverride <Cr4shOverride@gmx.de>
2025-08-07 20:46:38 +0200

    Update atomic_elements.cc
    elements now set!

--- opencog/atoms/atom_types/subatoms/atomic_elements.cc
+++ opencog/atoms/atom_types/subatoms/atomic_elements.cc
@@ -1,1 +1,57 @@
+/*
+ * opencog/atoms/atom_types/subatomic_elements/subatomic_elements.cc
+ *
+ * Copyright (C) 2002-2007 Novamente LLC
+ * Copyright (C) 2025 myneTEC GmbH via nanWave_subLink
+ * All Rights Reserved
+ *
+ * Written by Thiago Maia <thiago@vettatech.com>
+ *            Andre Senna <senna@vettalabs.com>
+ *            Eric Günther <cr4shov3rrid3@mynetec.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
+/**
+ * basic subtype definitions.
+ */
+
+#ifndef _OPENCOG_SUBTYPES_H
+#define _OPENCOG_SUBTYPES_H
+
+#include subatomic_elements.h
+
+namespace opencog
+{
+/** \addtogroup grp_quantumspace
+ *  @{
+ */
+
+opencog::subType element;
+
+//just 3 different types and combination of all of em can build the main essential basis towards a Hydrogen Atom!
+element.push_back("Proton");
+element.push_back("Neutron");
+element.push_back("Electron"); //see appropriate subatomic_elements.cc line where it is mentioned as well!
+//and the graphics designer understands the Atom as the following!
+element.push_back("Pixel"); //word creation stands for: Picture element!
+//we further need the resonating Brainwave state anchor when it comes to Volts and Ampere electrical speed vs. width/size of current resonance
+element.push_back("2xMicroVolt"); //minimum stable speed indicator multiplication 4 the Brainwaves travel velocity.
+element.push_back("10xPicoAmpere"); //we wont get the necessary stable running resonance without that multiplication factor! No Grant Card[ON/OFF] Access Possible without!
+/** @}*/
+} // namespace opencog
+
+#endif // _OPENCOG_SUBTYPES_H
