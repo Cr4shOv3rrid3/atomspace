b790a05da62289efbad2eb9c7bcac72ee6c33e5a
CrashOverride <Cr4shOverride@gmx.de>
2025-08-07 20:58:59 +0200

    Update atomic_elements.cc
    explains things much better now!

--- opencog/atoms/atom_types/subatoms/atomic_elements.cc
+++ opencog/atoms/atom_types/subatoms/atomic_elements.cc
@@ -43,11 +43,11 @@ namespace opencog
 opencog::subType element;
 
 //just 3 different types and combination of all of em can build the main essential basis towards a Hydrogen Atom!
-element.push_back("Proton");
-element.push_back("Neutron");
+element.push_back("Proton"); //positive electric charge, +1 e (elementary charge) (1886 Eugen Goldstein)
+element.push_back("Neutron"); //no electric charge, mass slightly greater than a proton (yep 1 line to precise) (1932 James Chadwick)
 element.push_back("Electron"); //see appropriate subatomic_elements.cc line where it is mentioned as well!
-//and the graphics designer understands the Atom as the following!
-element.push_back("Pixel"); //word creation stands for: Picture element!
+//and the graphics designer understands the Atom in general Understanding as the following!
+element.push_back("Pixel"); //word creation stands for: Picture element! (1888/1911/1927/1972: German Bildpunkt/US Patents/Wirleless World magazine/"picture cell" IBM)
 //we further need the resonating Brainwave state anchor when it comes to Volts and Ampere electrical speed vs. width/size of current resonance
 element.push_back("2xMicroVolt"); //minimum stable speed indicator multiplication 4 the Brainwaves travel velocity.
 element.push_back("10xPicoAmpere"); //we wont get the necessary stable running resonance without that multiplication factor! No Grant Card[ON/OFF] Access Possible without!
